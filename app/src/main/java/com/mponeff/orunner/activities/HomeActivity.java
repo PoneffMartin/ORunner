3813c0d257c2f62d8d073a2d2c256941621385a8
Martin Ponev <mponev@mm-sol.com>
2018-09-21 11:16:38 +0300

    Add user profile picture placeholder

--- app/src/main/java/com/mponeff/orunner/activities/HomeActivity.java
+++ app/src/main/java/com/mponeff/orunner/activities/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.mponeff.orunner.activities;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -19,6 +21,10 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.mponeff.orunner.R;
@@ -48,7 +54,6 @@ public class HomeActivity extends AppCompatActivity {
     TextView mToolbarTitle;
     @BindView(R.id.fab)
     FloatingActionButton mFab;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,6 +119,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private void setProfileInfo(NavigationView navigationView) {
         View headerView = navigationView.inflateHeaderView(R.layout.nav_header);
+        TextView profilePicPlaceholder = (TextView) headerView.findViewById(R.id.profile_picture_placeholder);
         TextView usernameTv = (TextView) headerView.findViewById(R.id.tv_username);
         TextView emailTv = (TextView) headerView.findViewById(R.id.tv_email);
         CircleImageView profileImage = (CircleImageView) headerView.findViewById(R.id.iv_profile_pic);
@@ -122,11 +128,25 @@ public class HomeActivity extends AppCompatActivity {
             Uri userPhoto = user.getPhotoUrl();
             String username = user.getDisplayName();
             String email = user.getEmail();
+            profilePicPlaceholder.setText(String.valueOf(username.charAt(0))); // TODO Consider exception handling
             usernameTv.setText(username);
             emailTv.setText(email);
             if (userPhoto != null) {
                 Glide.with(this)
                         .load(userPhoto)
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                profilePicPlaceholder.setVisibility(View.GONE);
+                                profileImage.setVisibility(View.VISIBLE);
+                                return false;
+                            }
+                        })
                         .into(profileImage);
             }
         }
