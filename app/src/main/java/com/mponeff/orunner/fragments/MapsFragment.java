5e2cdc623bea4da30527f3e11e684c72caf9dc1f
Martin Ponev <mponev@mm-sol.com>
2018-09-21 10:00:45 +0300

    Refactor MapsFragment

--- app/src/main/java/com/mponeff/orunner/fragments/MapsFragment.java
+++ app/src/main/java/com/mponeff/orunner/fragments/MapsFragment.java
@@ -5,16 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.mponeff.orunner.R;
 import com.mponeff.orunner.activities.ViewMapActivity;
@@ -32,23 +28,14 @@ public class MapsFragment extends Fragment {
 
     @BindView(R.id.rv_maps)
     RecyclerView mRecyclerView;
-    @BindView(R.id.rl_no_maps)
-    RelativeLayout mRlNoMaps;
-    @BindView(R.id.main_toolbar)
-    Toolbar mToolbar;
-    @BindView(R.id.add_activity_button)
-    Button mAddActivityBtn;
+    @BindView(R.id.ll_no_maps)
+    LinearLayout mLlNoMaps;
 
     private MapsAdapter mAdapter;
-
-    public static Fragment getInstance() {
-        return new MapsFragment();
-    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
         MapViewModel mapsViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
         mapsViewModel.getMaps().observe(this, maps -> {
             this.showMaps(maps);
@@ -61,7 +48,6 @@ public class MapsFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_maps_history, container, false);
         ButterKnife.bind(this, rootView);
-        setToolbar();
 
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -76,54 +62,30 @@ public class MapsFragment extends Fragment {
         mAdapter.setOnItemClickListener(new MapsAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(Map map) {
-                Bundle outState = new Bundle();
-                outState.putParcelable("data", map);
-                Intent viewMap = new Intent(getActivity(), ViewMapActivity.class);
-                viewMap.putExtras(outState);
-                startActivity(viewMap);
+                showMap(map);
             }
         });
 
         mRecyclerView.setAdapter(mAdapter);
-
-        mAddActivityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSportsDialog();
-            }
-        });
-
         return rootView;
     }
 
     public void showMaps(List<Map> maps) {
         if (maps.isEmpty()) {
             mRecyclerView.setVisibility(View.GONE);
-            mRlNoMaps.setVisibility(View.VISIBLE);
+            mLlNoMaps.setVisibility(View.VISIBLE);
         } else {
-            mRlNoMaps.setVisibility(View.GONE);
+            mLlNoMaps.setVisibility(View.GONE);
             mRecyclerView.setVisibility(View.VISIBLE);
             mAdapter.replaceAll(maps);
         }
     }
 
-    private void setToolbar() {
-        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
-        appCompatActivity.setSupportActionBar(mToolbar);
-        appCompatActivity.getSupportActionBar().setDisplayShowTitleEnabled(false);
-        TextView mTitle = (TextView) mToolbar.findViewById(R.id.toolbar_title);
-        mTitle.setText("Maps");
-        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().finish();
-            }
-        });
-    }
-
-    private void showSportsDialog() {
-        ChooseTypeDialog sportsDialog = new ChooseTypeDialog();
-        sportsDialog.show(getFragmentManager(), null);
+    private void showMap(Map map) {
+        Bundle outState = new Bundle();
+        outState.putParcelable("data", map);
+        Intent viewMap = new Intent(getActivity(), ViewMapActivity.class);
+        viewMap.putExtras(outState);
+        startActivity(viewMap);
     }
 }
